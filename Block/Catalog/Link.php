d5c737e7d2a6aaf4b5e9f20406fe47af7c13541d
nachorios <riosignacio312@gmail.com>
2021-08-12 21:02:42 -0300

    comit

---
+++ Block/Catalog/Link.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Bootcamp\StockStatus\Block\Catalog;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+class Link extends \Magento\Catalog\Block\Product\View
+{
+
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\Url\EncoderInterface $urlEncoder,
+        \Magento\Framework\Json\EncoderInterface $jsonEncoder,
+        \Magento\Framework\Stdlib\StringUtils $string,
+        \Magento\Catalog\Helper\Product $productHelper,
+        \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig,
+        \Magento\Framework\Locale\FormatInterface $localeFormat,
+        \Magento\Customer\Model\Session $customerSession,
+        ProductRepositoryInterface $productRepository,
+        \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency,
+        array $data = [],
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+    ) {
+
+        $this->_scopeConfig = $scopeConfig;
+        $this->stockRegistry = $stockRegistry;
+
+        parent::__construct(
+            $context,
+            $urlEncoder,
+            $jsonEncoder,
+            $string,
+            $productHelper,
+            $productTypeConfig,
+            $localeFormat,
+            $customerSession,
+            $productRepository,
+            $priceCurrency,
+            $data
+        );
+    }
+
+    public function isActive(){
+        return $this->_scopeConfig->getValue('stock_status/settings/enable');
+    }
+/*
+    public function getInStock(){
+        return $this->_scopeConfig->getValue('stock_status/quanty/in_stock');
+    }
+
+    public function getLowStock(){
+        return $this->_scopeConfig->getValue('stock_status/quanty/low_stock');
+    }
+
+    public function getOutOfStock(){
+        return $this->_scopeConfig->getValue('stock_status/quanty/out_of_stock');
+    }
+
+    public function getProductId()
+    {
+        return $this->getProduct()->getId();
+    }
+
+    public function getProductQty()
+    {
+        $productStock = $this->stockRegistry->getStockItem($this->getProduct()->getId());
+        $productQty = $productStock->getQty();
+        return $productQty;
+    }*/
+
+    public function getQty(){
+        $productStock = $this->stockRegistry->getStockItem($this->getProduct()->getId());
+        $productQty = $productStock->getQty();
+        $inStock = $this->_scopeConfig->getValue('stock_status/quanty/in_stock');
+        $lowStock = $this->_scopeConfig->getValue('stock_status/quanty/low_stock');
+        $outStock = $this->_scopeConfig->getValue('stock_status/quanty/out_of_stock');
+        if( $productQty >=  $inStock){
+            $label =  '<span class="status-stock status-in-stock">'.__('In Stock').'</span>';
+            
+        }
+        elseif($productQty >=  $lowStock){
+            $label =  '<span class="status-stock status-low-stock">'.__('Low Stock').'</span>';
+            
+        }
+        else{
+            $label =  '<span class="status-stock status-out-of-stock">'.__('Out of Stock').'</span>';
+            
+        }
+        return $label;
+    }
+
+}
